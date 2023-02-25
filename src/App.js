20562d1702a8bf0d799ecec93ea448b0cf4ed599
jchaz22 <jason.chaskin@gmail.com>
2023-02-25 13:29:18 -0700

    added hey

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import { useState } from "react"
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [currentAccount, setCurrentAccount] = useState("")
+  const [currentAccount, setCurrentAccount] = useState("");
 
   async function connectWallet() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = accounts[0]
-    setCurrentAccount(account)
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = accounts[0];
+    setCurrentAccount(account);
   }
 
   return (
     <div className="app">
       <div className="header">
         <h1>ens subdomain infra</h1>
-        <button className="header-button" onClick={connectWallet}>{currentAccount ? "connected" : "connect wallet"}</button>
+        <h1>Hey</h1>
+        <button className="header-button" onClick={connectWallet}>
+          {currentAccount ? "connected" : "connect wallet"}
+        </button>
       </div>
       <p className="address-div">{currentAccount ? currentAccount : ""}</p>
     </div>
-  )
+  );
 }
 
 export default App;
