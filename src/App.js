627263753b821caea263a65409720ed3a2540df7
0xgeeb <0xgeeb@gmail.com>
2023-02-25 13:02:09 -0700

    setup connect button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
+
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState("")
+
+  async function connectWallet() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const account = accounts[0]
+    setCurrentAccount(account)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <h1>ens subdomain infra</h1>
+        <button className="header-button" onClick={connectWallet}>{currentAccount ? "connected" : "connect wallet"}</button>
+      </div>
+      <p className="address-div">{currentAccount ? currentAccount : ""}</p>
     </div>
-  );
+  )
 }
 
 export default App;
